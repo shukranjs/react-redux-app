46a935a064366e357742db8d833251d09c236713
shukranjsp@gmail.com <shukranjsp@gmail.com>
2024-03-18 22:24:22 +0100

    redux setup

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from "redux";
+
+const countReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state; // This should be outside the if statements
+};
+
+const store = createStore(countReducer);
+
+export default store;
